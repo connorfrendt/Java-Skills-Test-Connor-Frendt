5983baf2f0cb714504ee59ba794eb5a48ed8c019
Connor Frendt <cjacksonfx@yahoo.com>
2019-03-26 09:46:35 -0700

    hard coded ingredients

---
+++ index.js
@@ -0,0 +1,1 @@
+import ingredients from './ingredients';
---
+++ ingredients.js
@@ -0,0 +1,10 @@
+const ingredients = {
+  strawberry: 500,
+  banana: 500,
+  mango: 500,
+  condensedMilk: 500,
+  ice: 500,
+  sugar: 200
+}
+
+export default ingredients;
