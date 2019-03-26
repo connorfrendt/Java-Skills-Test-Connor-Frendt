6677026068df19b7f90a5b2eaff3edaf7a81d1b1
Connor Frendt <cjacksonfx@yahoo.com>
2019-03-26 11:25:10 -0700

    able to update current inventory

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import ingredients from './ingredients';
+const ingredients = require('./ingredients.js');
+
+ingredients.strawberry -= 150;
+console.log(ingredients);
+if(/* strawberry smoothie */) {
+  ingredients.strawberry -= 150;
+  ingredients.condensedMilk -= 60;
+  ingredients.ice -= 90;
+  ingredients.sugar -= 24;
+  if(ingredients.strawberry < 150 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
+    return 'Sale denied. Not enough ingredients to make smoothie';
+  }
+}
+
--- ingredients.js
+++ ingredients.js
@@ -1,10 +1,17 @@
+var strawberryGrams = 500;
+var bananaGrams = 500;
+var mangoGrams = 500;
+var milkML = 500;
+var iceML = 500;
+var sugarGrams = 200;
+
 const ingredients = {
-  strawberry: 500,
-  banana: 500,
-  mango: 500,
-  condensedMilk: 500,
-  ice: 500,
-  sugar: 200
+  strawberry: strawberryGrams,
+  banana: bananaGrams,
+  mango: mangoGrams,
+  condensedMilk: milkML,
+  ice: iceML,
+  sugar: sugarGrams
 }
 
-export default ingredients;
+module.exports = ingredients;
