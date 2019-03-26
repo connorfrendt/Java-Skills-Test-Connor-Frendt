fd71a8debe08a739e79a62aae14e291cec2fb2d4
Connor Frendt <cjacksonfx@yahoo.com>
2019-03-26 13:31:54 -0700

    able to interact with command line and shoots you back to menu once you have reached the end!

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
 const ingredients = require('./ingredients.js');
 const inquirer = require('inquirer');
 
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'selection',
-    message: 'What kind of smoothie would you like? (Strawberry, Banana, Mango)',
-    choices: [{
-      name: 'Strawberry'
-    },
+const menu = () => {
+
+  inquirer.prompt([
     {
-      name: 'Banana'
-    },
-    {
-      name: 'Mango'
-    }]
-  }
-])
-  .then(({ selection }) => {
-    if(selection === 'Banana') {
-      console.log('You ordered bananas!');
+      type: 'list',
+      name: 'selection',
+      message: 'What kind of smoothie would you like? (Strawberry, Banana, Mango)',
+      choices: [{
+        name: 'Strawberry'
+      },
+      {
+        name: 'Banana'
+      },
+      {
+        name: 'Mango'
+      }]
     }
-  })
+  ])
+    .then(({ selection }) => {
+      if(selection === 'Strawberry') {
+        
+        if(ingredients.strawberry < 150) {
+          console.log('Sale denied.  Not enough ingredients to make smoothie');
+        }
+  
+        ingredients.strawberry -= 150;
+        console.log('You have ', ingredients, ' in your inventory left!');
+      }
+    })
+    .then(() => {
+      return menu();
+    })
+}
 
-// ingredients.strawberry -= 150;
-// console.log(ingredients);
-// if(/* strawberry smoothie */) {
-//   ingredients.strawberry -= 150;
-//   ingredients.condensedMilk -= 60;
-//   ingredients.ice -= 90;
-//   ingredients.sugar -= 24;
-//   if(ingredients.strawberry < 150 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
-//     return 'Sale denied. Not enough ingredients to make smoothie';
-//   }
-// }
+menu();
 
-// if(/* banana smoothie */) {
-//   ingredients.banana -= 180;
-//   ingredients.condensedMilk -= 60;
-//   ingredients.ice -= 90;
-//   ingredients.sugar -= 24;
 
-//   if(ingredients.banana < 180 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
-//     return 'Sale denied.  Not enough ingredients to make smoothie';
-//   }
-// }
 
-// if(/* mango smoothie */) {
-//   ingredients.mango -= 210;
-//   ingredients.condensedMilk -= 60;
-//   ingredients.ice -= 90;
-//   ingredients.sugar -= 24;
 
-//   if(ingredients.mango < 210 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
-//     return 'Sale denied.  Not enough ingredients to make smoothie';
-//   }
-// }
+
+
