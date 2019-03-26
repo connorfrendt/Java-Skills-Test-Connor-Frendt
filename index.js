c5f02ec2dcf7da270d060e0169db3ec80d1af398
Connor Frendt <cjacksonfx@yahoo.com>
2019-03-26 14:18:46 -0700

    refactored ingredients and working on second page for menu

--- index.js
+++ index.js
@@ -7,43 +7,110 @@ const menu = () => {
     {
       type: 'list',
       name: 'selection',
-      message: 'What kind of smoothie would you like? (Strawberry, Banana, Mango)',
-      choices: [{
-        name: 'Strawberry'
-      },
+      message: '\n\n What kind of smoothie would you like? \n',
+      choices: [
       {
-        name: 'Banana'
-      },
-      {
-        name: 'Mango'
-      }]
+        name: 'Check Inventory'
+      }
+      // {
+      //   name: 'Strawberry'
+      // },
+      // {
+      //   name: 'Banana'
+      // },
+      // {
+      //   name: 'Mango'
+      // }
+    ]
     }
   ])
     .then(({ selection }) => {
-      if(selection === 'Strawberry') {
-        
-        if(ingredients.strawberry < 150 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
-          console.log('Sale denied.  Not enough ingredients to make smoothie');
-        } else {
-          ingredients.strawberry -= 150;
-          ingredients.condensedMilk -= 60;
-          ingredients.ice -= 90;
-          ingredients.sugar -= 24;
 
-          console.log('\n');
-          console.log('***INVENTORY***')
-          console.log('You have ', ingredients.strawberry, ' STRAWBERRIES left!');
+      if(selection === 'Check Inventory') {
+        console.log('\n');
+          console.log('*** INVENTORY ***');
+          console.log('You have ', ingredients.strawberry, 'grams of STRAWBERRIES left!');
+          console.log('You have ', ingredients.banana, 'grams of BANANAS left!');
+          console.log('You have ', ingredients.mango, 'grams of MANGOS left!');
           console.log('You have ', ingredients.condensedMilk, 'mL of CONDENSED MILK left!');
           console.log('You have ', ingredients.ice, 'mL of ICE left!');
           console.log('You have ', ingredients.sugar, 'grams of SUGAR left!');
-          
-        }
-  
+          console.log('\n');
       }
 
-      if(selection === 'Banana') {
-        if(ingredients.banana < 180)
-      }
+
+      // if(selection === 'Strawberry') {
+        
+      //   if(ingredients.strawberry < 150 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
+      //     console.log('Sale denied.  Not enough ingredients to make smoothie');
+      //   }
+        
+      //   else {
+      //     ingredients.strawberry -= 150;
+      //     ingredients.condensedMilk -= 60;
+      //     ingredients.ice -= 90;
+      //     ingredients.sugar -= 24;
+
+      //     console.log('\n');
+      //     console.log('*** INVENTORY ***');
+      //     console.log('You have ', ingredients.strawberry, 'grams of STRAWBERRIES left!');
+      //     console.log('You have ', ingredients.banana, 'grams of BANANAS left!');
+      //     console.log('You have ', ingredients.mango, 'grams of MANGOS left!');
+      //     console.log('You have ', ingredients.condensedMilk, 'mL of CONDENSED MILK left!');
+      //     console.log('You have ', ingredients.ice, 'mL of ICE left!');
+      //     console.log('You have ', ingredients.sugar, 'grams of SUGAR left!');
+      //     console.log('\n');
+
+      //   }
+      // }
+
+      // if(selection === 'Banana') {
+
+      //   if(ingredients.banana < 180 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
+      //     console.log('Sale denied.  Not enough ingredients to make smoothie');
+      //   }
+        
+      //   else {
+      //     ingredients.banana -= 180;
+      //     ingredients.condensedMilk -= 60;
+      //     ingredients.ice -= 90;
+      //     ingredients.sugar -= 24;
+
+      //     console.log('\n');
+      //     console.log('*** INVENTORY ***');
+      //     console.log('You have ', ingredients.strawberry, ' STRAWBERRIES left!');
+      //     console.log('You have ', ingredients.banana, ' BANANAS left!');
+      //     console.log('You have ', ingredients.mango, ' MANGOS left!');
+      //     console.log('You have ', ingredients.condensedMilk, 'mL of CONDENSED MILK left!');
+      //     console.log('You have ', ingredients.ice, 'mL of ICE left!');
+      //     console.log('You have ', ingredients.sugar, 'grams of SUGAR left!');
+      //     console.log('\n');
+
+      //   }
+      // }
+
+      // if(selection === 'Mango') {
+      //   if(ingredients.mango < 180 || ingredients.condensedMilk < 60 || ingredients.ice < 90 || ingredients.sugar < 24) {
+      //     console.log('Sale denied.  Not enough ingredients to make smoothie');
+      //   } else {
+      //     ingredients.mango -= 180;
+      //     ingredients.condensedMilk -= 60;
+      //     ingredients.ice -= 90;
+      //     ingredients.sugar -= 24;
+
+      //     console.log('\n');
+      //     console.log('*** INVENTORY ***');
+      //     console.log('You have ', ingredients.strawberry, ' STRAWBERRIES left!');
+      //     console.log('You have ', ingredients.banana, ' BANANAS left!');
+      //     console.log('You have ', ingredients.mango, ' MANGOS left!');
+      //     console.log('You have ', ingredients.condensedMilk, 'mL of CONDENSED MILK left!');
+      //     console.log('You have ', ingredients.ice, 'mL of ICE left!');
+      //     console.log('You have ', ingredients.sugar, 'grams of SUGAR left!');
+      //     console.log('\n');
+
+      //   }
+      // }
+
     })
     .then(() => {
       return menu();
